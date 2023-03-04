e4ac41d6bb3e6f67daa416ba60430b28cd2a9619
Marcel Tricolici <marcel@tricolici.com>
2023-03-04 13:00:15 +0200

    install ruby for errbit with rvm

--- my-cookbooks/errbit/attributes/ruby.rb
+++ my-cookbooks/errbit/attributes/ruby.rb
@@ -3,7 +3,8 @@
 default['errbit']['ruby']['version']='2.7.2'
 
 # requirements needed to install a specific ruby version with rvm
-default['errbit']['ruby']['dependencies'] = ['build-essential']
+# TODO: use metapackage build-essentials + what's missing
+default['errbit']['ruby']['dependencies'] = %w[gawk autoconf automake bison libffi-dev libgdbm-dev libncurses5-dev libsqlite3-dev libtool libyaml-dev pkg-config sqlite3 zlib1g-dev libgmp-dev libreadline-dev libssl-dev g++ make]
 
 default['errbit']['rvm']['keyserver'] = 'keyserver.ubuntu.com'
 default['errbit']['rvm']['keys'] = '409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB'
--- my-cookbooks/errbit/recipes/ruby.rb
+++ my-cookbooks/errbit/recipes/ruby.rb
@@ -12,12 +12,24 @@ end
 user = data_bag_item(node['errbit']['users']['databag'], node['errbit']['users']['id'])
 
 bash 'install RVM' do
-  user 'errbit'
+  user user['id']
+  group user['groups'][0]
   login true
   creates "#{user['home']}/.rvm/bin/rvm"
   code <<-EOH
     gpg --keyserver #{node['errbit']['rvm']['keyserver']} --recv-keys #{node['errbit']['rvm']['keys']}
     curl -sSL https://get.rvm.io | bash -s stable
+  EOH
+  action :run
+end
+
+bash 'install ruby' do
+  user user['id']
+  group user['groups'][0]
+  login true
+  code <<-EOH
+    source ~/.profile
+    rvm install #{node['errbit']['ruby']['version']}
   EOH
   action :run
 end
